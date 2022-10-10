c804cc7461a8fe4cb95b66cd0418ebe82b9c5eed
ankushlohiya001 <ankushlohiya2000@gmail.com>
2022-10-10 23:28:06 +0530

    written linked list and item class

--- src/output.java
+++ src/output.java
@@ -1,62 +1,32 @@
-class Juicer{
-	// fixed props
-	String nameOfJuicer = "Juci"; // name of brand
-	int capacity = 1; // capacity in litres
-	int factor = 5;
-	int minSpeed = 0;
-	int maxSpeed = 100;
-	
-	// states
-	boolean isOn = false; // whatever juicer is on/off
-	int speed = 0; // speed in rpm
-	
-	void powerOn(){
-		isOn = true;
-	}
+package src;
 
-	void powerOff(){
-		isOn = false;
-		speed = 0;
-	}
-
-	void incSpeed(){
-		if(speed + factor <= maxSpeed){
-			speed += factor;
-		}else{
-			System.out.println("speed can't increased further");
-		}
-	}
-
-	void decSpeed(){
-		if(speed - factor >= minSpeed){
-			speed -= factor;
-		}else{
-			System.out.println("speed can't decreased further");
-		}
-	}
-	
-	void showStatus(){
-		if(isOn){
-			System.out.println("Juicer "+ nameOfJuicer +" is on and running at "+ speed +" rpm.");
-		}else{
-			System.out.println("Juicer "+ nameOfJuicer +" is off");
-		}
-	}
+// Item to be used in linked list
+class Item {
+  int value; // value of item
+  Item next; // reference to next item
 }
 
-class Test{
-	public static void main (String[] args){
-		Juicer j1 = new Juicer();
-		j1.nameOfJuicer = "ssd";
+class LList {
+  Item head; // starting point / item
+  Item tail; // ending tail
+  // push -> insert element/item at last
 
-		Juicer j2 = new Juicer();
-		j2.nameOfJuicer = "hdd";
+  void push(int value) {
+    Item tmp = new Item();
+    tmp.value = value;
+    head.next = tmp;
+  }
+  // pop -> remove element/item at last
+  // size -> length of linked list
 
-		j1.powerOn();
-		j1.decSpeed();
-		System.out.println(j1.speed);
+}
 
-		//j1.showStatus();
-		//j2.showStatus();
-	}
+class Test {
+  public static void main(String[] args) {
+    Item head = new Item();
+    head.value = 56;
+    head.next = new Item();
+    head.next.value = 67;
+    System.out.println(head.next.value);
+  }
 }
